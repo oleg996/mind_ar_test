70d73a38b2fb2c9e43f1105c7950813c6ae4cb9f
oleg996 <oleg1bagan@gmail.com>
2025-04-21 13:21:29 +0200

    tidyup

--- input.js
+++ input.js
@@ -47,15 +47,15 @@ AFRAME.registerComponent("input", {
 
   handleRotation: function (event) {
 
-    var x_cos = Math.cos( this.modelViewer.object3D.rotation.z);
-    var y_sin = Math.sin( this.modelViewer.object3D.rotation.z);
+    var x_m = Math.cos( this.modelViewer.object3D.rotation.z);
+    var y_m = Math.sin( this.modelViewer.object3D.rotation.z);
 
     if (this.isVisible) {
 
       this.el.object3D.position.x +=
-        (event.detail.positionChange.x*x_cos+event.detail.positionChange.y*y_sin) * this.data.rotationFactor;
+        (event.detail.positionChange.x*x_m+event.detail.positionChange.y*y_m) * this.data.rotationFactor;
       this.el.object3D.position.z +=
-      (event.detail.positionChange.y*x_cos+event.detail.positionChange.x*y_sin) * this.data.rotationFactor;
+      (event.detail.positionChange.y*x_m+event.detail.positionChange.x*y_m) * this.data.rotationFactor;
     }
   },
 
