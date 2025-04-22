1f36d567abed2fb433d9545749b37c8106c44049
oleg996 <oleg1bagan@gmail.com>
2025-04-22 09:45:48 +0200

    what

--- rotator.js
+++ rotator.js
@@ -1,8 +1,10 @@
 AFRAME.registerComponent("rotator", {
 
     init: function(){
+
         var intervalId = window.setInterval(function(){
-            this.el.object3D.rotation.z += 1;
+             this.el.object3D.rotation.z += 1;
+             
           }, 30);
 
 
